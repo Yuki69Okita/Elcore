a749af2a8e419a4082f12c408b482b02c2146fc6
Valentina Marinova <61772169+Yuki69Okita@users.noreply.github.com>
2024-01-31 12:04:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//Active nab-bar item
+const navLinkEls = document.querySelectorAll('.nav-link');
+const windowPathname = window.location.pathname;
+
+navLinkEls.forEach(navLinkEl => {
+    const navLinkPathname = new URL(navLinkEl.href).pathname;
+    
+    if (windowPathname === navLinkPathname) {
+        navLinkEl.classList.add('active_link');
+    }
+});
+
+
+//Sticky nav
+const nav = document.querySelector('nav');
+window.addEventListener("scroll", function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+    }
+    else{
+        nav.classList.remove("sticky");
+    }
+})
